b46e29649a2c10c1ac7a2b10a2dc4eb6032da71d
Stefan Koch <11819754+StefKode@users.noreply.github.com>
2022-06-19 17:21:13 +0200

    make read string-safe

--- examples/auto_turnoff/auto_turnoff.py
+++ examples/auto_turnoff/auto_turnoff.py
@@ -92,7 +92,7 @@ report("started")
 while True:
     for dev in devMgr.Devices:
         if USE_REDIS:
-            timelimit = bool(red.get(dbconf.redis_aclimit_key))
+            timelimit = bool(int(red.get(dbconf.redis_aclimit_key)))
             dev.evaluate(api, report, timelimit)
         else:
             dev.evaluate(api, report)
